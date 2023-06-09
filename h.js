5c140121ddaf3a60f10a7c1e39914d7b6601eaae
sonukolAi <136089232+sonukolAi@users.noreply.github.com>
2023-06-09 21:48:56 +0530

    Update h.js

--- h.js
+++ h.js
@@ -5,68 +5,62 @@ const cheerio = require('cheerio');
 const app = express();
 const port = process.env.PORT || 3330;
 
-app.get('/:num', async (req, res) => {
-  try {
-    const { num } = req.params;
-    const url = `https://www.google.com/search?q=${num}+train+running+status`;
-    const { data } = await axios.get(url, {
-      headers: {
-        'User-Agent':
-        'Mozilla/5.0 (Linux; Android 11; Pixel 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Mobile Safari/537.36',
-      },
-    });
-    const $ = cheerio.load(data);
-    const trainName = $('div.k9rLYb').eq(0).text().trim();
-    const liveStatus = $('div.dK1Bub .rUtx7d').eq(1).text();
-    const delayTime = $('div.Rjvkvf.MB86Dc').eq(1).text().trim();
-    const modifiedData = { trainName, liveStatus, delayTime };
-    res.json(modifiedData);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error occurred');
-  }
-});
+const sources = {
+  '0': {
+    url: (num) => `https://www.google.com/search?q=${num}+train+running+status`,
+    processData: ($, res) => {
+      const train = $('div.k9rLYb').eq(0).text().trim();
+      const status = $('div.dK1Bub .rUtx7d').eq(1).text();
+      const delay = $('div.Rjvkvf.MB86Dc').eq(1).text().trim();
+      const currentTime = new Date().toLocaleTimeString('en-US', { hour12: false, hour: 'numeric', minute: 'numeric' });
+      res.json({ train, status, delay, currentTime });
+    },
+  },
+  '1': {
+    url: (num) => `https://runningstatus.in/status/${num}`,
+    processData: ($, res) => {
+      const train = $('head').text().replace('Live Train Running Status', '').trim();
+      const station = $('.table-success').text().trim();
+      const currentTime = new Date().toLocaleTimeString('en-US', { hour12: false, hour: 'numeric', minute: 'numeric' });
+      res.json({ train, station, currentTime });
+    },
+  },
+  '2': {
+    url: (num) => `https://trainstatus.com/runningstatus/${num}`,
+    processData: ($, res) => {
+      const train = $('head').text().replace('Live Train Running Status', '').trim();
+      const station = $('.panel-heading').text().trim();
+      const currentTime = new Date().toLocaleTimeString('en-US', { hour12: false, hour: 'numeric', minute: 'numeric' });
+      res.json({ train, station, currentTime });
+    },
+  },
+  '3': {
+    url: (num) => `https://spotyourtrain.com/trainstatus?train=${num}`,
+    processData: ($, res) => {
+      const train = $('head').text().replace('Live Train Running Status', '').trim();
+      const station = $('.table-success').text().trim();
+      const currentTime = new Date().toLocaleTimeString('en-US', { hour12: false, hour: 'numeric', minute: 'numeric' });
+      res.json({ train, station, currentTime });
+    },
+  },
+};
 
-app.get('/:num/1', async (req, res) => {
+app.get('/:num/:source', async (req, res) => {
   try {
-    const { num } = req.params;
-    const url = `https://runningstatus.in/status/${num}`;
-    const { data } = await axios.get(url);
-    const $ = cheerio.load(data);
-    const trainName = $('head').text().replace('Live Train Running Status', '').trim();
-    const currentStation = $('.table-success').text().trim();
-    const modifiedData = { Name: trainName, currentStation: currentStation };
-    res.json(modifiedData);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error occurred');
-  }
-});
-
-app.get('/:num/2', async (req, res) => {
-  try {
-    const { num } = req.params;
-    const { data } = await axios.get(`https://trainstatus.com/runningstatus/${num}`);
-    const $ = cheerio.load(data);
-    const trainName = $('head').text().replace('Live Train Running Status', '').trim();
-    const currentStation = $('.panel-heading').text().trim();
-    const modifiedData = { Name: trainName, currentStation: currentStation };
-    res.json(modifiedData);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error occurred');
-  }
-});
-
-app.get('/:num/3', async (req, res) => {
-  try {
-    const { num } = req.params;
-    const { data } = await axios.get(`https://spotyourtrain.com/trainstatus?train=${num}`);
-    const $ = cheerio.load(data);
-    const trainName = $('head').text().replace('Live Train Running Status', '').trim();
-    const currentStation = $('.table-success').text().trim();
-    const modifiedData = { Name: trainName, currentStation: currentStation };
-    res.json(modifiedData);
+    const { num, source } = req.params;
+    const selectedSource = sources[source];
+    if (selectedSource) {
+      const url = selectedSource.url(num);
+      const { data } = await axios.get(url, {
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Linux; Android 11; Pixel 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Mobile Safari/537.36',
+        },
+      });
+      const $ = cheerio.load(data);
+      selectedSource.processData($, res);
+    } else {
+      res.status(400).send('Invalid source');
+    }
   } catch (error) {
     console.error(error);
     res.status(500).send('Error occurred');
