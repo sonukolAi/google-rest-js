40b703b8a660591d07e0e461ee64f30ee36a994d
sonukolAi <136089232+sonukolAi@users.noreply.github.com>
2023-07-13 14:40:24 +0530

    Update h.js

--- h.js
+++ h.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const app = express();
-const port = process.env.PORT || 3330;
+const port = process.env.PORT || 3000;
 
 const sources = {
   '0': {
